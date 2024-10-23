26c000fd3fcd928dfe570dad35224f36462df9c8
suarez112 <esuarez14@udi.edu.co>
2024-10-23 16:53:43 -0500

    Cuarto Avance

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,11 +6,8 @@ export const routes: Routes = [
     {path: "", component: LoginComponent},
     {path: "Gerente",loadComponent: () => import('./Gerente/main2.component'),
         children: [
-            { path: "ConsultarLotes",loadComponent: () => import('./consultar-lotes/consultar-lotes.component')},
+            
             { path: "ConsultarProveedor", loadComponent:() => import('./consultar-proveedor/consultar-proveedor.component')},
-            { path: "ConsultarProducto", loadComponent: () => import('./consultar-productos-existencia/consultar-productos-existencia.component')},
-            { path: "ModificarProducto", loadComponent: () => import('./modificar-producto/modificar-producto.component')},
-            { path: "ModificarLotes", loadComponent: () => import('./modificar-lote/modificar-lote.component')},
             { path: "GuardarProducto", loadComponent: () => import('./guardar-producto/guardar-producto.component')},
             { path: "GuardarLotes", loadComponent: () => import('./guardar-lote/guardar-lote.component')},
             { path: "GenerarInforme", loadComponent: () => import('./generar-informe/generar-informe.component')}
@@ -20,9 +17,9 @@ export const routes: Routes = [
     {path: "Admin", loadComponent: () =>import('./Admin/main.component'),
         children:[
             { path: "ConsultarLotes",loadComponent: () => import('./consultar-lotes/consultar-lotes.component')},
-            { path: "ConsultarProveedor", loadComponent:() => import('./consultar-proveedor/consultar-proveedor.component')},
+            
             { path: "ConsultarProducto", loadComponent: () => import('./consultar-productos-existencia/consultar-productos-existencia.component')},
-            { path: "ModificarProveedor", loadComponent: () => import('./modificar-proveedor/modificar-proveedor.component')},
+            
             { path: "GuardarProveedor", loadComponent: () => import('./guardar-proveedor/guardar-proveedor.component')},
             { path: "GenerarInforme", loadComponent: () => import('./generar-informe/generar-informe.component')}
         ]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,20 +1,33 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  constructor(private router: Router){
+  username: string = '';
+  password: string = '';
 
-  }
-  ingresar(){
-  this.router.navigateByUrl("Admin");
-  
-  }
-  }
+  constructor(private authService: AuthService,private router: Router) {}
 
+  ingresar(): void {
+    this.authService.login(this.username, this.password).subscribe(
+      response => {
+        console.log('Login successful', response);
+        this.router.navigateByUrl("Admin");
+        //localStorage.setItem('token', response.token);        
+        //this.router.navigateByUrl("Gerente");
+      },
+      error => {
+        console.error('Login failed', error);
+      }
+    );
+  }
+}
